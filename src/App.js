31a2e59bb8a6f08a8760da1669fdb7fe38da42f2
shifas <shifas@ecodons.com>
2023-02-18 20:02:13 +0530

    winning squares highlight

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import  './App.css';
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick, isWinner }) {
     return (
-        <button className="square" onClick={onSquareClick}>
+        <button className={isWinner ? 'square winning-square' : 'square'} onClick={onSquareClick}>
             {value}
         </button>
     );
@@ -26,7 +26,7 @@ function Board({ xIsNext, squares, onPlay }) {
     const winner = calculateWinner(squares);
     let status;
     if (winner) {
-        status = 'Winner: ' + winner;
+        status = 'Winner: ' + squares[winner[0]];
     } else {
         status = 'Next player: ' + (xIsNext ? 'X' : 'O');
     }
@@ -38,7 +38,11 @@ function Board({ xIsNext, squares, onPlay }) {
                 return <div className="board-row">
                     {[0,1,2].map(col => {
                         const i = row * 3 + col;
-                        return <Square value={squares[i]} onSquareClick={() => handleClick(i)} />;
+                        return <Square
+                            value={squares[i]}
+                            onSquareClick={() => handleClick(i)}
+                            isWinner={winner && winner.includes(i)}
+                        />;
                     })}
                 </div>
             })}
@@ -114,7 +118,7 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+            return lines[i];
         }
     }
     return null;
