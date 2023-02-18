1c1fd04efad5d43706c9e534694c63d160e60925
shifas <shifas@ecodons.com>
2023-02-18 19:46:28 +0530

    tic tac toe init

--- src/App.js
+++ src/App.js
@@ -34,21 +34,14 @@ function Board({ xIsNext, squares, onPlay }) {
     return (
         <>
             <div className="status">{status}</div>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-                <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-                <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-                <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-                <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-                <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-                <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-            </div>
+            {[0, 1 ,2].map(row => {
+                return <div className="board-row">
+                    {[0,1,2].map(col => {
+                        const i = row * 3 + col;
+                        return <Square value={squares[i]} onSquareClick={() => handleClick(i)} />;
+                    })}
+                </div>
+            })}
         </>
     );
 }
@@ -69,19 +62,30 @@ export default function Game() {
         setCurrentMove(nextMove);
     }
 
-    const moves = history.map((squares, move) => {
+    const [sortAscending, setMovesAsc] = useState(true);
+
+    let moves = history.map((squares, move) => {
         let description;
-        if (move > 0) {
+        if (move === currentMove) {
+            description = 'You are at move #' + move;
+        } else if (move > 0) {
             description = 'Go to move #' + move;
         } else {
             description = 'Go to game start';
         }
         return (
             <li key={move}>
-                <button onClick={() => jumpTo(move)}>{description}</button>
+                {move === currentMove ? description : (
+                    <button onClick={() => jumpTo(move)}>{description}</button>
+                )}
             </li>
         );
     });
+
+
+    moves = sortAscending
+        ? moves
+        : moves.slice().reverse();
 
     return (
         <div className="game">
@@ -89,6 +93,7 @@ export default function Game() {
                 <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
             </div>
             <div className="game-info">
+                <button onClick={() => setMovesAsc(!sortAscending)}>{sortAscending ? 'ASC' : 'DESC' }</button>
                 <ol>{moves}</ol>
             </div>
         </div>
